564db956fa4bde1d1290dcf48771b24bcef63d30
Hallory <khalpeep@gmail.com>
2026-05-09 17:33:38 +0200

    add manager login funtion

--- t300/conftest.py
+++ t300/conftest.py
@@ -1,9 +1,18 @@
 import pytest
+from data.constants import BASE_URL, MANAGER_USER
 from pages.app import App
-from data.constants import BASE_URL
 
 
 @pytest.fixture(scope="function")
 def app(page):
+    page.set_viewport_size({"width": 1366, "height": 768})
     page.goto(BASE_URL)
     return App(page)
+
+
+@pytest.fixture(scope="function")
+def manager_app(app):
+    app.auth.login(MANAGER_USER["email"], MANAGER_USER["password"])
+    app.auth.should_be_logged_in()
+
+    return app
