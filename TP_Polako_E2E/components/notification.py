34b12b9528a650109798b8a842da9453f55c8e66
Hallory <khalpeep@gmail.com>
2026-05-28 17:08:15 +0200

    delete another command files

--- TP_Polako_E2E/components/notification.py
+++
@@ -1,5 +0,0 @@
-from playwright.sync_api import Page, expect
-
-
-class NotificationComponent:
-    pass
--- TP_Polako_E2E/pages/ticket/cart_page.py
+++
@@ -1,5 +0,0 @@
-from TP_Polako_E2E.base.base_page import BasePage
-
-
-class CartPage(BasePage):
-    pass
